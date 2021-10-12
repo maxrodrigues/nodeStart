4bb146375f3a4c7eff045eba2c278d21c95a12f3
Maxuel Rodrigues <maxuel.rodrigues@gmail.com>
2021-10-12 17:38:21 -0300

    create update script

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -65,6 +65,7 @@ exports.getByTags = (req, res, next) => {
         });
 };
 
+// Create new product
 exports.post = (req, res, next) => {
     let product = new Product(req.body);
     product
@@ -83,9 +84,25 @@ exports.post = (req, res, next) => {
 };
 
 exports.put = (req, res, next) => {
-    const id = req.params.id;
-
-    res.status(201).send({ id: id, item: req.body });
+    Product.findByIdAndUpdate(req.params.id, {
+        $set: {
+            title: req.body.title,
+            description: req.body.description,
+            price: req.body.price,
+            slug: req.body.slug,
+        },
+    })
+        .then((x) => {
+            res.status(200).send({
+                message: "Produto atualizado com sucesso",
+            });
+        })
+        .catch((e) => {
+            res.status(400).send({
+                message: "Houve um erro ao atualizar o produto",
+                data: e,
+            });
+        });
 };
 
 exports.delete = (req, res, next) => {
